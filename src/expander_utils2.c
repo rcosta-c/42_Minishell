1c021d3bd5d62699a51c8fa04cad5547afb958d3
rcosta-c <ruben.carvalho@gmail.com>
2025-01-28 09:01:27 +0000

    update v13.6 (norm correct)

--- src/expander_utils2.c
+++ src/expander_utils2.c
@@ -6,22 +6,53 @@
 /*   By: rcosta-c <rcosta-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 17:18:39 by rcosta-c          #+#    #+#             */
-/*   Updated: 2025/01/20 22:11:53 by rcosta-c         ###   ########.fr       */
+/*   Updated: 2025/01/28 09:00:05 by rcosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*expand_token_seeker2(t_sh *sh, int *x, int n, char *c)
+static char	*ft_copy_name_helper(t_sh *sh, int *x, int n)
+{
+	int		z;
+	char	temp[500];
+
+	z = 0;
+	temp[z] = sh->tokens[n].tokens[*x];
+	(*x)++;
+	z++;
+	while (sh->tokens[n].tokens[*x])
+	{
+		if (ft_isalnum(sh->tokens[n].tokens[*x]) == 0)
+			break ;
+		temp[z] = sh->tokens[n].tokens[*x];
+		(*x)++;
+		z++;
+	}
+	temp[z] = '\0';
+	return (ft_strdup(temp));
+}
+
+static char	*ft_get_my_pid(t_sh *sh, int *x)
 {
 	pid_t	pid;
 
-	if (sh->tokens[n].tokens[*x] == '$' && sh->tokens[n].tokens[*x + 1] == '$')
+	(void)sh;
+	(*x) += 2;
+	pid = getpid();
+	return (ft_itoa(pid));
+}
+
+char	*expand_token_seeker2(t_sh *sh, int *x, int n, char *c)
+{
+	if ((*x) > 0)
 	{
-		(*x) += 2;
-		pid = getpid();
-		return (ft_itoa(pid));
+		if (sh->tokens[n].tokens[*x] == '$'
+			&& sh->tokens[n].tokens[*x - 1] == 92)
+			return (ft_copy_name_helper(sh, x, n));
 	}
+	if (sh->tokens[n].tokens[*x] == '$' && sh->tokens[n].tokens[*x + 1] == '$')
+		return (ft_get_my_pid(sh, x));
 	if (sh->tokens[n].tokens[*x] == '$' && sh->tokens[n].tokens[*x + 1] == '?')
 	{
 		(*x) += 2;
